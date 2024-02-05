6b5274c0a19230f229cb2642ffbfcd9521c241a6
José Chi <a17000597@alumnos,uady.mx>
2024-02-04 21:19:20 -0600

    prueba 4

--- api/index.js
+++ api/index.js
@@ -138,8 +138,8 @@ function getFileType(extension) {
 }
 
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
 
 module.exports = app;
