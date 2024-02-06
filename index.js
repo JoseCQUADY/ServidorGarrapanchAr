a7f637fa1194c0a184e876d5402894d53ad74bdb
José Chi <a17000597@alumnos,uady.mx>
2024-02-05 18:02:17 -0600

    firebase prueba 2

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage, ref ,uploadBytes} from "firebase/storage";
-import firebaseConfig from './firebase.config';
 
 const express = require('express');
 const multer = require('multer');
@@ -11,10 +10,20 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 const dotenv = require('dotenv').config();
-const firebaseapp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseapp);
-const storageFB = getStorage();
 
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const storageFB = getStorage();
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 app.use(cors({origin: '*'}));
 
